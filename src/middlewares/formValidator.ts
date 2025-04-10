81d11597c5944c17858d68a276e2600eccd52232
bosukeme <ukeme.wilson@gmail.com>
2025-04-10 12:42:00 +0100

    fix joi bug

--- src/middlewares/formValidator.ts
+++ src/middlewares/formValidator.ts
@@ -1,7 +1,7 @@
 import Joi from "joi";
 
 export const spamFormSchema = Joi.object({
-    spamNumber: Joi.number().integer().min(1).max(20).required(),
-    nglName: Joi.string().required(),
-    textInput: Joi.string().min(1).required()
+  spamNumber: Joi.string().required(),
+  nglName: Joi.string().required(),
+  textInput: Joi.string().min(1).required(),
 });
